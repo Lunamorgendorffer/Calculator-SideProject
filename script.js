4de92642dda4b00099b0c93e3c4ef2b2ac8f42f2
Lunamorgendorffer <nabdou68@gmail.com>
2023-05-19 15:28:26 +0200

    fonc inputDecimal et handleOperator

--- script.js
+++ script.js
@@ -17,4 +17,45 @@ function inputDigit(digit) {
     }
 }
 
+function inputDecimal(dot) {
+    if (calculator.waitingForSecondOperand === true) { // Si on attend le deuxième opérande
+        calculator.displayValue = "0."; // Mettre la valeur affichée à "0."
+        calculator.waitingForSecondOperand = false; // Réinitialiser l'indicateur waitingForSecondOperand à false
+        return; // Sortir de la fonction
+    }
+  
+    if (!calculator.displayValue.includes(dot)) { // Si le symbole décimal n'est pas déjà présent dans la valeur affichée
+        calculator.displayValue += dot; // Ajouter le symbole décimal à la fin de la valeur affichée
+    }
+}
 
+function handleOperator(nextOperator) {
+    const { firstOperand, displayValue, operator } = calculator; // Destructuration des propriétés de l'objet calculator
+    const inputValue = parseFloat(displayValue); // Convertir la valeur affichée en nombre à virgule flottante
+    
+    if (operator && calculator.waitingForSecondOperand)  { // Si un opérateur est déjà défini et qu'on attend le deuxième opérande
+        calculator.operator = nextOperator; // Mettre à jour l'opérateur avec le prochain opérateur
+        return; // Sortir de la fonction
+    }
+  
+    if (firstOperand == null && !isNaN(inputValue)) { // Si le premier opérande est null et que inputValue est un nombre valide
+        calculator.firstOperand = inputValue; // Mettre à jour le premier opérande avec la valeur de inputValue
+    } else if (operator) { // Sinon, si un opérateur est déjà défini
+        const result = calculate(firstOperand, inputValue, operator); // Calculer le résultat en utilisant la fonction calculate() avec les opérandes et l'opérateur actuels
+  
+        calculator.displayValue = `${parseFloat(result.toFixed(7))}`; // Mettre à jour la valeur affichée avec le résultat arrondi à 7 décimales
+        calculator.firstOperand = result; // Mettre à jour le premier opérande avec le résultat
+    }
+  
+    calculator.waitingForSecondOperand = true; // Mettre waitingForSecondOperand à true pour indiquer qu'on attend le deuxième opérande
+    calculator.operator = nextOperator; // Mettre à jour l'opérateur avec le prochain opérateur
+}
+
+
+
+
+
+
+  
+
+  
